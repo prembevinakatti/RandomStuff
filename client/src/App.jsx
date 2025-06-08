3b554eb0828c0ea11aa73747c298269428251a1b
prem <prem@prem.com>
2025-06-08 12:54:04 +0530

    ...

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import AppRoutes from "./Routes/AppRoutes";
+
+function App() {
+  return (
+    <>
+      <AppRoutes />
+    </>
+  );
+}
+export default App;
---
+++ client/src/Routes/AppRoutes.jsx
@@ -0,0 +1,13 @@
+import Login from "@/components/Auth/Login";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  );
+};
+
+export default AppRoutes;
