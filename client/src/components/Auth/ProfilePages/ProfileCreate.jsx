a8b7320ba7a53c0faa3d656c80708237a8683776
prem <prem@prem.com>
2025-08-03 09:57:51 +0530

    ..

--- client/src/components/Auth/ProfilePages/ProfileCreate.jsx
+++ client/src/components/Auth/ProfilePages/ProfileCreate.jsx
@@ -256,7 +256,7 @@ const ProfileCreate = () => {
   };
 
   return (
-    <div className="min-h-screen bg-[#0f0f0f] text-white flex items-center justify-center px-4 relative overflow-hidden">
+    <div className="min-h-screen bg-[#0f0f0f] text-white flex items-center py-16 justify-center px-4 relative overflow-hidden">
       <div className="absolute inset-0 bg-[radial-gradient(circle,rgba(255,255,255,0.05)_1px,transparent_1px)] [background-size:20px_20px] pointer-events-none z-0" />
 
       <motion.div
